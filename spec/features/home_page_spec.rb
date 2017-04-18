2763a37d4723eb77cc099281f667c6f161525636
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-04-18 18:13:40 +0100

    test for profile displayed when sign up/in

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -7,4 +7,17 @@ feature 'landing page' do
       expect(page).to have_content 'Social Network'
     end
   end
+
+  context 'profile page' do
+    scenario 'should display profile page when signed up' do
+      visit '/users/sign_up'
+      click_button 'Sign up'
+      fill_in 'Email', with: '1@2.3.com'
+      fill_in 'Username', with: 'Test'
+      fill_in 'Password', with: '111111'
+      fill_in 'Password confirmation', with: '111111'
+      click_button 'Sign up'
+      expect(page).to have_content 'Welcome Test'
+    end
+  end
 end
