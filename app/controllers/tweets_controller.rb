c79e754739a5ccbb0a963a1dc086c84fe5e71f9e
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-04-18 19:05:24 +0100

    migrations for adding tweets/user id

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,14 @@
+class TweetsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    current_user.tweets.create(message: tweet_params[:message], user_id: current_user.id)
+    redirect_to root_path
+  end
+
+  private
+
+    def tweet_params
+      params.require(:tweet).permit(:message, :user_id)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418162018) do
+ActiveRecord::Schema.define(version: 20170418180315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "message"
+    t.string   "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
