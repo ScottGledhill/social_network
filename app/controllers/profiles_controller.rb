01be1c62313a1e224723912b996ed4bc3c6451db
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-04-18 20:55:18 +0100

    username route added for search, factory girl gem added

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,6 @@ class ProfilesController < ApplicationController
   def show
     @current_user = current_user
     @user = User.find(params[:id])
+    @following = current_user.followees.include? @user.id.to_s
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :profiles, only: [:show]
   resources :tweets, only: [:create]
   resources :followees, only: [:create, :destroy]
-  resources :search
+  get 'search/username' => 'search#username'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
