7a204302311724de9159e12c4a35eaac9309f02b
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-04-19 11:48:47 +0100

    test for search function

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -20,4 +20,24 @@ feature 'landing page' do
       expect(page).to have_content 'Welcome Test'
     end
   end
+
+  scenario 'should be able to search for users' do
+    visit '/users/sign_up'
+    click_button 'Sign up'
+    fill_in 'Email', with: '1@2.3.com'
+    fill_in 'Username', with: 'Test'
+    fill_in 'Password', with: '111111'
+    fill_in 'Password confirmation', with: '111111'
+    click_button 'Sign up'
+    click_link 'Sign Out'
+    click_link 'Sign Up'
+    fill_in 'Email', with: 'new@2.3.com'
+    fill_in 'Username', with: 'New User'
+    fill_in 'Password', with: '111111'
+    fill_in 'Password confirmation', with: '111111'
+    click_button 'Sign up'
+    fill_in 'username', with: 'Test'
+    click_button 'Search'
+    expect(page).to have_content 'Test'
+  end
 end
--- spec/features/profile_page_spec.rb
+++ spec/features/profile_page_spec.rb
@@ -15,17 +15,4 @@ feature 'profile page' do
       expect(page).to have_content 'Test Name'
     end
   end
-
-  scenario 'should display tweets from username' do
-    visit '/users/sign_up'
-    click_button 'Sign up'
-    user = create(:user)
-    fill_in 'Email', with: '1@2.3.com'
-    fill_in 'Username', with: 'Test'
-    fill_in 'Password', with: '111111'
-    fill_in 'Password confirmation', with: '111111'
-    click_button 'Sign up'
-    visit profile_path(user)
-    expect(page).to have_content ''
-  end
 end
